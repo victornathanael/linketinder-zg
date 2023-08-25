655e49b49733909846ca888a39c59d7a065c573e
Victor Nathanael <victornathanael15@gmail.com>
2023-08-24 22:36:44 -0300

    refactor: add saveCandidate method and moved NewCandidate for view package

--- src/model/CandidateDAO.groovy
+++ src/model/CandidateDAO.groovy
@@ -54,35 +54,7 @@ class CandidateDAO {
             )
     ]
 
-    static void newCandidate() {
-        Scanner input = new Scanner(System.in)
-
-        print " Digite o nome do novo candidato: "
-        String name = input.nextLine()
-
-        print " Digite o email do novo candidato: "
-        String email = input.nextLine()
-
-        print " Digite o cpf do novo candidato: "
-        String cpf = input.nextLine()
-
-        print " Digite a idade do novo candidato: "
-        int age = input.nextInt()
-        input.nextLine()
-
-        print " Digite o estado do novo candidato: "
-        String state = input.nextLine()
-
-        print " Digite o CEP do novo candidato: "
-        String cep = input.nextLine()
-
-        print " Digite a descrição do novo candidato: "
-        String personalDescription = input.nextLine()
-
-        print " Digite as competências do novo candidato (ex: java, angular, groovy): "
-        String skills = input.nextLine()
-
-
+    static void saveCandidate(String name, String email, String cpf, int age, String state, String cep, String personalDescription, String skills) {
         candidates.add(new Candidate(
                 name: name,
                 email: email,
