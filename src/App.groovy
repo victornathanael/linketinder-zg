013c1d3cfdddada835ca29ac020bd77bf7b50d62
Victor Nathanael <victornathanael15@gmail.com>
2023-10-05 21:08:33 -0300

    feat: add loop menu to candidate, company, job and skill

--- src/App.groovy
+++ src/App.groovy
@@ -1,43 +1,191 @@
-import util.ClearConsole
-import util.Menu
-import view.ListCandidates
-import view.ListCompanies
-import view.NewCandidate
-import view.NewCompany
+import util.*
+
+import view.Candidates.*
+import view.Companies.*
+import view.Jobs.*
+import view.Skills.*
 
 class App {
     static void main(String[] args) {
         Scanner scanner = new Scanner(System.in)
+
+        loopMenuPrincipal(scanner)
+    }
+
+    static void loopMenuPrincipal(Scanner scanner) {
         while (true) {
+            Menu.menuPrincipal()
 
-            Menu.menu()
-
-            def choice
+            int choice;
             try {
-                choice = scanner.nextInt()
+                choice = scanner.nextInt();
             } catch (InputMismatchException ignored) {
-                println "Digite um número válido"
+                println("Digite um número válido")
                 scanner.nextLine()
                 ClearConsole.clearConsole()
                 continue
             }
 
             switch (choice) {
-                case 1 -> ListCandidates.listCandidates()
-                case 2 -> ListCompanies.listCompanies()
-                case 3 -> NewCandidate.newCandidate()
-                case 4 -> NewCompany.newCompany()
-                case 5 -> {
+                case 1:
+                    loopMenuCandidato(scanner)
+                    break
+                case 2:
+                    loopMenuEmpresa(scanner)
+                    break
+                case 3:
+                    loopMenuVagas(scanner)
+                    break
+                case 4:
+                    loopMenuCompetencias(scanner)
+                    break
+                case 5:
                     ClearConsole.clearConsole()
-                    println "Saindo..."
-                }
-                default -> {
+                    println("Saindo...")
+                    return
+                default:
                     ClearConsole.clearConsole()
-                    println "Digite um número válido"
-                }
+                    println("Digite um número válido")
             }
-            if (choice == 5) {
-                break;
+        }
+    }
+
+    static void loopMenuCandidato(Scanner scanner) {
+        while (true) {
+            Menu.menuCandidato()
+
+            int choice;
+            try {
+                choice = scanner.nextInt();
+            } catch (InputMismatchException ignored) {
+                println("Digite um número válido")
+                scanner.nextLine()
+                ClearConsole.clearConsole()
+                continue
+            }
+
+            switch (choice) {
+                case 1:
+                    ListCandidates.listCandidates()
+                    break
+                case 2:
+                    NewCandidate.newCandidate()
+                    break
+                case 3:
+                    UpdateCandidate.updateCandidate()
+                    break
+                case 4:
+                    DeleteCandidate.deleteCandidate()
+                    break
+                case 5:
+                    return
+                default:
+                    println("Digite um número válido")
+            }
+        }
+    }
+
+    static void loopMenuEmpresa(Scanner scanner) {
+        while (true) {
+            Menu.menuEmpresa()
+
+            int choice;
+            try {
+                choice = scanner.nextInt();
+            } catch (InputMismatchException ignored) {
+                println("Digite um número válido")
+                scanner.nextLine()
+                ClearConsole.clearConsole()
+                continue;
+            }
+
+            switch (choice) {
+                case 1:
+                    ListCompanies.listCompanies()
+                    break
+                case 2:
+                    NewCompany.newCompany()
+                    break
+                case 3:
+                    UpdateCompany.updateCompany()
+                    break
+                case 4:
+                    DeleteCompany.deleteCompany()
+                    break
+                case 5:
+                    return
+                default:
+                    println("Digite um número válido")
+            }
+        }
+    }
+
+    static void loopMenuVagas(Scanner scanner) {
+        while (true) {
+            Menu.menuVaga()
+
+            int choice;
+            try {
+                choice = scanner.nextInt();
+            } catch (InputMismatchException ignored) {
+                println("Digite um número válido")
+                scanner.nextLine()
+                ClearConsole.clearConsole()
+                continue
+            }
+
+            switch (choice) {
+                case 1:
+                    ListJobs.listJobs()
+                    break
+                case 2:
+                    NewJob.newJob()
+                    break
+                case 3:
+                    UpdateJob.updateJob()
+                    break
+                case 4:
+                    DeleteJob.deleteJob()
+                    break
+                case 5:
+                    return
+                default:
+                    println("Digite um número válido")
+            }
+        }
+    }
+
+    static void loopMenuCompetencias(Scanner scanner) {
+        while (true) {
+            Menu.menuCompetencia()
+
+            int choice;
+            try {
+                choice = scanner.nextInt();
+            } catch (InputMismatchException ignored) {
+                println("Digite um número válido")
+                scanner.nextLine()
+                ClearConsole.clearConsole()
+                continue
+            }
+
+            switch (choice) {
+                case 1:
+                    ListSkills.listSkills()
+                    break
+                case 2:
+                    NewSkill.newSkill()
+                    break
+                case 3:
+                    UpdateSkill.updateSkill()
+                    break
+                case 4:
+                    DeleteSkill.deleteSkill()
+                    break
+                case 5:
+                    return
+                default:
+                    println("Digite um número válido")
             }
         }
     }
