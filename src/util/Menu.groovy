080e51b1111f369537a39d6f71bdad28bc2bb999
Victor Nathanael <victornathanael15@gmail.com>
2023-10-05 21:07:34 -0300

    feat: add menu

--- src/util/Menu.groovy
+++ src/util/Menu.groovy
@@ -1,13 +1,61 @@
 package util
 
-static void menu() {
-    print """
+static void menuPrincipal() {
+    print("""
     ======== MENU ==========
-    1 - Listar os candidatos
-    2 - Listar as empresas
-    3 - Cadastrar um novo candidato
-    4 - Cadastrar uma nova empresa
+    1 - Candidato
+    2 - Empresa
+    3 - Vaga
+    4 - Competência
     5 - Sair
 
-Escolha uma opção: """
+Escolha uma opção: """)
+}
+
+static void menuCandidato() {
+    print("""
+    ======== MENU CANDIDATO ==========
+    1 - Listar candidatos
+    2 - Cadastrar novo candidato
+    3 - Alterar dados de um candidato
+    4 - Deletar um candidato
+    5 - Voltar ao menu principal
+
+Escolha uma opção: """)
+}
+
+static void menuEmpresa() {
+    print("""
+    ======== MENU EMPRESA ==========
+    1 - Listar empresas
+    2 - Cadastrar nova empresa
+    3 - Alterar dados de uma empresa
+    4 - Deletar uma empresa
+    5 - Voltar ao menu principal
+
+Escolha uma opção: """)
+}
+
+static void menuVaga() {
+    print("""
+    ======== MENU VAGA ==========
+    1 - Listar vagas
+    2 - Cadastrar nova vaga
+    3 - Alterar dados de uma vaga
+    4 - Deletar uma vaga
+    5 - Voltar ao menu principal
+
+Escolha uma opção: """)
+}
+
+static void menuCompetencia() {
+    print("""
+    ======== MENU COMPETÊNCIA ==========
+    1 - Listar competências
+    2 - Cadastrar nova competência
+    3 - Alterar dados de uma competência
+    4 - Deletar uma competência
+    5 - Voltar ao menu principal
+
+Escolha uma opção: """)
 }
